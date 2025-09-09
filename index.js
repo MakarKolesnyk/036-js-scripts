ed55ea06748c6b5ec88fc0fc375cfeaa68ee5400
Makar <kolesnykmakar0@gmail.com>
2025-09-10 00:17:19 +0300

    let. const

---
+++ index.js
@@ -0,0 +1,42 @@
+console.log('hi!');
+
+const isWork = false;
+
+const myHeight = 1.80;
+console.log(myHeight);
+
+const isHaveICar = false;
+
+const timeWakeUp = 8;
+console.log(timeWakeUp);
+
+const amountDaysInWeek  = 7;
+console.log(amountDaysInWeek );
+
+
+const titleMovie = 'Intertail';
+console.log(titleMovie);
+// titleMovie = 'Titanic';
+
+let myPet = 'dog, cat';
+console.log(myPet);
+
+let firstName = 'Makar';
+console.log(firstName);
+
+let isAdult = false;
+let isWeekend = false;
+let isStudent = true;
+
+let yesrOfBirth = 1900;
+console.log(yesrOfBirth);
+yesrOfBirth = 2010;
+console.log(yesrOfBirth);
+
+let myFavoriteColor = 'black';
+console.log(firstName, myFavoriteColor);
+
+let age = null;
+console.log(age);
+age = 15;
+console.log(age);
