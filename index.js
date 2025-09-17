eae195ac7e0810b440d070def05d5cc7fd165297
Makar <kolesnykmakar0@gmail.com>
2025-09-17 19:00:42 +0300

    if, else, logical operators

--- index.js
+++ index.js
@@ -1,57 +1,84 @@
-if (confirm("You want convert from USD to UAH?")) {
-  const howMuchUsdYouWantConvert = prompt("How much USD you Want Convert");
-  const courseUSD = prompt('Write course USD')
-  const howMuchUahHaveUser = Number(howMuchUsdYouWantConvert) * courseUSD;
-  alert("In UAH you have -" + howMuchUahHaveUser + "!");
-} else {
- const howMuchUahYouWantConvert = prompt("How much UAH you Want Convert");
- const courseUSD = prompt('Write course USD')
- const howMuchUahHaveUser = Number(howMuchUahYouWantConvert) / courseUSD;
- alert("In USD you have - " + howMuchUahHaveUser+"!")
-}
 
-// let amount = 0;
 
-// if (confirm("Is the Sun a star?")) {
-//   amount++;
+
+// const userWriteNumber = prompt("Write your number");
+// const userNumber = Number(userWriteNumber);
+
+// if (userNumber === 0) {
+//   alert("Zero")
+// } else if (userNumber > 0) {
+//   alert("Positive Numbers")
+// } else (userNumber < 0) {
+//   alert("Negative Numbers")
 // }
 
-// if (!confirm("Does the Moon orbit Mars?")) {
-//   amount++;
-// }
 
-// if (confirm("Does Saturn have rings?")) {
-//   amount++;
-// }
+// const accsesPassword = "qwe123";
+// const userPassword = prompt("Write your passoword");
 
-// if (!confirm("Is Jupiter the smallest planet in the Solar System?")) {
-//   amount++;
-// }
-
-// if (confirm("Can astronauts float in space because of microgravity?")) {
-//   amount++;
-// }
-
-// alert("Congratulations your score - " + amount + "!");
-
-// const whatUserWantForBD = prompt('what you want for birth day?')
-// const isWhatUserWantForBD = confirm('This what you want for birth day - ' + whatUserWantForBD + '!')
-
-// if (whatUserWantForBD) {
-//     alert('Good! I go find your present')
+// if (userPassword === accsesPassword) {
+//   alert("Entry allowed");
 // } else {
-//     alert('Think again')
+//   alert("Entry denied");
 // }
 
-// const userName = prompt('Enter your name');
-// const isUserName = confirm('This your name - ' + userName + '?')
+// const accsesAge = 12;
+// const userAge = prompt("How old are you?")
 
-// console.log(userName);
-// console.log(isUserName);
+// if (Number(userAge) >= accsesAge) {
+//   alert("You can buy ticket")
+// }
+// else{
+//   const yearWait = accsesAge - Number(userAge)
+//   alert("You need wait year " + yearWait)
+// }
 
-// if (isUserName) {
-//     alert('Hi, ' + userName + '!');
+// const num1User = prompt("Write first number")
+// const num2user = prompt("Write second number")
+
+// const num1 = Number(num1User)
+// const num2 = Number(num2user)
+
+// if (num1 > num2) {
+//   alert(num1 +  '- biggest')
+// } else if(num2 > num1){
+//   alert(num2 + ' - biggest')
 // } else {
-//     alert('next time be careful!')
+//   alert('Equal')
 // }
 
+// const num1 = 5;
+
+// const num2 = 8;
+
+// const str1 = '5';
+// const str2 = 'cat';
+// const str3 = 'dog';
+// const str4 = 'cat';
+// const str5 = 'Cat';
+
+// console.log(str2 > str3);
+// console.log(str5 > str3);
+// console.log(str2 > str5);
+
+// console.log(str2 === str3);
+// console.log(str2 === str4);
+// console.log(str2 === str5);
+
+// console.log(num1 === str1);
+// console.log(num1 === Number(str1));
+
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+
+// console.log(num3 >= num2);
+// console.log(num3 <= num2);
+
+// console.log(num1 === num2);
+// console.log(num3 === num2);
+
+// console.log(num1 !== num2);
+// console.log(num3 !== num2);
