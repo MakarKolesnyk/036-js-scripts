bbc5cf11956fea0af18ce64eb106d1105e938789
Makar <kolesnykmakar0@gmail.com>
2025-09-17 19:13:08 +0300

    add Remainder

--- index.js
+++ index.js
@@ -1,84 +1,20 @@
+const number = 452;
+const remainder = number%2;
 
+if (remainder === 0) {
+   console.log('doubles, even');
+} else {
+  console.log('unpaired, odd');
+  
+}
 
+const number7 = 452;
+const remainder7 = number%7;
 
-// const userWriteNumber = prompt("Write your number");
-// const userNumber = Number(userWriteNumber);
-
-// if (userNumber === 0) {
-//   alert("Zero")
-// } else if (userNumber > 0) {
-//   alert("Positive Numbers")
-// } else (userNumber < 0) {
-//   alert("Negative Numbers")
-// }
-
-
-// const accsesPassword = "qwe123";
-// const userPassword = prompt("Write your passoword");
-
-// if (userPassword === accsesPassword) {
-//   alert("Entry allowed");
-// } else {
-//   alert("Entry denied");
-// }
-
-// const accsesAge = 12;
-// const userAge = prompt("How old are you?")
-
-// if (Number(userAge) >= accsesAge) {
-//   alert("You can buy ticket")
-// }
-// else{
-//   const yearWait = accsesAge - Number(userAge)
-//   alert("You need wait year " + yearWait)
-// }
-
-// const num1User = prompt("Write first number")
-// const num2user = prompt("Write second number")
-
-// const num1 = Number(num1User)
-// const num2 = Number(num2user)
-
-// if (num1 > num2) {
-//   alert(num1 +  '- biggest')
-// } else if(num2 > num1){
-//   alert(num2 + ' - biggest')
-// } else {
-//   alert('Equal')
-// }
-
-// const num1 = 5;
-
-// const num2 = 8;
-
-// const str1 = '5';
-// const str2 = 'cat';
-// const str3 = 'dog';
-// const str4 = 'cat';
-// const str5 = 'Cat';
-
-// console.log(str2 > str3);
-// console.log(str5 > str3);
-// console.log(str2 > str5);
-
-// console.log(str2 === str3);
-// console.log(str2 === str4);
-// console.log(str2 === str5);
-
-// console.log(num1 === str1);
-// console.log(num1 === Number(str1));
-
-// console.log(num1 > num2);
-// console.log(num1 < num2);
-
-// console.log(num1 >= num2);
-// console.log(num1 <= num2);
-
-// console.log(num3 >= num2);
-// console.log(num3 <= num2);
-
-// console.log(num1 === num2);
-// console.log(num3 === num2);
-
-// console.log(num1 !== num2);
-// console.log(num3 !== num2);
+if (remainder7 === 0) {
+   console.log('multiply 7');
+} else {
+  console.log('not multiply 7');
+  
+}
+ 
