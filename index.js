a4d20a711e60041f5cc67fed5d3b0622b8dacc50
Makar <kolesnykmakar0@gmail.com>
2025-09-17 20:04:00 +0300

    Logical AND, OR

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
 // false
 // undefiend
-console.log(Boolean(undefined)); // false
 // null
-console.log(Boolean(null)); // false
 // '' "" ``
-console.log(Boolean('')); // false
 // 0 +0 -0
-console.log(Boolean(0)); // false
-// NaN 
-console.log(Boolean(NaN));
+// NaN
 
+// const userAgeInput = prompt("Enter your age")
+// const userAge= Number(userAgeInput);
+// const startSchoolAge = 6;
+// const endSchoolAge = 17;
 
-// ! NOT
-console.log(!false); //true
-console.log(!true); //false
+// if (userAge>=startSchoolAge && userAge<=ndSchoolAge) {
+//  alert('school age')
+// } else {
+//   alert("Not school age")
+// }
 
-const str = '';
- console.log(!str); //true !false
+// const userNumberInput = prompt("Enter Number");
+// const userNumber = Number(userNumberInput);
+// const remainder3 = userNumber%3;
+// const remainder7 = userNumber%7;
 
- console.log(!undefined); // false -> true
- 
- 
+// if (remainder3 === 0 && remainder7 === 0) {
+//   alert("You WIN");
+// } else {
+//   alert("You LOSE");
+// }
+
+const userInput = prompt("Enter hours");
+console.log(userInput);
+const userNumber = Number(userInput); // NaN
+console.log(userNumber);
+
+// user enter cancel
+const error1 = (userInput = null);
+// user send empty string
+const error2 = userInput === "";
+// user write letters instead of numbers
+const error3 = Number.isNan(userNumber) === true;
+
+if (error1 || error2 || error3) {
+  alert("Error.\n" + userInput + "- not a number");
+} else {
+  if (userNumber >= 0 && userNumber <= 23) {
+    alert("thanks");
+  } else {
+    alert("Error.\n" + userInput + " - not a hours");
+  }
+}
