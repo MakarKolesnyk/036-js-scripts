8c7616e9a35110abc5fdadd30704631892eb3f06
Makar <kolesnykmakar0@gmail.com>
2025-09-29 19:14:36 +0300

    Add switch

--- index.js
+++ index.js
@@ -1,52 +1,38 @@
-const userFisrtInput = prompt("enter 1 side")
-const userSecondInput = prompt("enter 2 side")
+const massage = `write language like want to speak
+Ukraine - ua
+English- en
+Poland - pl
+Spainish - es
+`;
 
-const titleFigure = (userFisrtInput === userSecondInput) ? 'square' : 'rectangle'
-alert(titleFigure)
-// const usetNumberInput = prompt("enter number")
-// const userNumber = Number(usetNumberInput)
-// const absoluteValue = (userNumber >= 0) ? userNumber : -userNumber;
-// alert(absoluteValue)
+const lang = prompt(massage);
 
+switch (lang) {
+  case "ua":
+    alert("Привiт");
+    break;
+  case "en":
+    alert("Hello");
+    break;
+  case "pl":
+    alert("Cześć");
+    break;
+  case "es":
+    alert("Hola");
+    break;
+  default:
+     alert("launguage not support");
+    break;
+}
 
-// const priceTicket = 100;
-
-// const userAgeInput = prompt("enter age")
-// const userAge = Number(userAgeInput);
-
-// const sale = (userAge < 18 || userAge >= 65) ? 10 : 0;
-
-// const summaCheck = priceTicket - (priceTicket*sale/100)
-
-// alert(summaCheck)
-// const userNumberinput = prompt("Enter Number")
-// let wordEvenOdd = (Number(userNumberinput)%2 === 0) ? 'even' : 'odd' ;
-
-// alert(userNumberinput + ' - ' + wordEvenOdd)
-
-// const summaInput = prompt("Enter summa");
-// const summa = Number(summaInput);
-// const minSumma = 1000;
-
-// const delivery = (summa > minSumma) ? 0 : 50;
-
-// if (summa >= minSumma) {
-//   delivery = 0;
+// if (lang === "ua") {
+//   alert("Привiт");
+// } else if (lang === "en") {
+//   alert("Hello");
+// } else if (lang === "pl") {
+//   alert("Cześć");
+// } else if (lang === "es") {
+//   alert("Hola");
 // } else {
-//   delivery = 50;
-// }
-
-// alert("For pay = " + (summa + delivery));
-// const userFisrtNumberInput = prompt("enter first number");
-// const userSecondNumberInput = prompt("enter second number");
-
-// const userFirstNumber = Number(userFisrtNumberInput);
-// const userSecondNumber = Number(userSecondNumberInput);
-
-// if (userFirstNumber > 0 && userSecondNumber > 0) {
-//   alert("This two posetive numbers")
-// } else if (userFirstNumber < 0 && userSecondNumber < 0) {
-//   alert("This negative numbers")
-// } else {
-//   alert("This difference numbers")
+//   alert("launguage not support");
 // }
