d1eb8637876fd6fea818240486b683bf34ad83e9
Makar <kolesnykmakar0@gmail.com>
2025-09-29 19:53:49 +0300

    Do task with switch

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
-switch (confirm("Do u want leave from site?")) {
-  case true:
-    alert("Goodbye");
+const userInput1 = prompt("Enter first number");
+const userInput2 = prompt("Enter second number");
+
+const number1 = Number(userInput1);
+const number2 = Number(userInput2);
+
+const operator = prompt(" Enter operator +,  -,  *,  /, %");
+
+switch (operator) {
+  case "+": {
+    const result = number1 + number2;
+    alert(number1 + "+" + number2 + "=" + result);
     break;
-  case false:
-    alert("Thanks");
+  }
+  case "-": {
+    const result = number1 - number2;
+    alert(number1 + "-" + number2 + "=" + result);
+    break;
+  }
+  case "*": {
+    const result = number1 * number2;
+    alert(number1 + "*" + number2 + "=" + result);
+    break;
+  }
+  case "/": {
+    const result = number1 / number2;
+    alert(number1 + "/" + number2 + "=" + result);
+    break;
+  }
+  case "%": {
+    const result = number1 % number2;
+    alert(number1 + "%" + number2 + "=" + result);
+    break;
+  }
   default:
     break;
 }
 
-confirm("Do u want leave from site?")
- ? alert("Goodbye")
- : alert("Thanks");
+// const userInputDayOfWeek = `Write day of week:
+// For 1 to 7`;
+
+// const dayOfWeek = prompt(userInputDayOfWeek);
+
+// switch (dayOfWeek) {
+//   case "1":
+//   case "2":
+//   case "3":
+//   case "4":
+//   case "5":
+//     alert("workday");
+//   case "6":
+//   case "7":
+//     alert("Weekend");
+//     break;
+//   default:
+//     alert("There is not day of week");
+//     break;
+// }
