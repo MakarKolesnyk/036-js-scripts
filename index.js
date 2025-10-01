3f709226056e784aca98f7c113ba5819e25f7351
Makar <kolesnykmakar0@gmail.com>
2025-10-01 10:39:31 +0300

    Add code about secret number

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
-let ammountPlate = 0;
-const totalAmmountPlate = 5;
-
-while (ammountPlate < totalAmmountPlate) {
-  ammountPlate++;
-  console.log("washing plate ", ammountPlate);
-}
-
-// while (ammountPlate>0) {
-//   console.log("Wahsing plate ", ammountPlate);
-//   ammountPlate++;
+// while (true) {
+//   const userNumberInput = prompt("Enter number 77");
+//   if (userNumberInput === "77") {
+//     alert("thanks");
+//     break;
+//   }
 // }
+
+// while (true) {
+//   const userNumberInput = prompt("Enter number multiply 7");
+//   const userNumber = Number(userNumberInput)
+//   if (userNumber % 7 === 0 && userNumberInput!=='' && userNumberInput!==null) {
+//     alert("thanks");
+//     break;
+//   }
+// }
+
+// while (true) {
+//   const userNumberInput = prompt("Enter number multiply 0");
+//   if (userNumber === '0') {
+//     alert("thanks");
+//     break;
+//   }
+// }
+
+// while (true) {
+//   const userInput = prompt("Enter something");
+//   if (userInput) {
+//     alert("thanks");
+//     break;
+//   }
+// }
+
+const secterNumber = 4;
+
+while (true) {
+  const userInput = prompt("Enter number");
+  const userNumber = Number(userInput);
+
+  if (userInput.trim()=== "" || userInput === null || Number.isNaN(userNumber)) {
+    alert("error");
+  } else if (userNumber === secterNumber) {
+    alert("you win!");
+    break;
+  } else if (userInput === "" || userInput === null) {
+    alert("error");
+  } else if (userNumber > secterNumber) {
+    alert("Secret number less");
+  } else {
+    alert("Secret Number bigger");
+  }
+}
