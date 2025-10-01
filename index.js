2f5c2bbbcb21990f061e23df0b71aa59c9e23b84
Makar <kolesnykmakar0@gmail.com>
2025-10-01 09:49:43 +0300

    add while with update

--- index.js
+++ index.js
@@ -1,58 +1,12 @@
-const userInput1 = prompt("Enter first number");
-const userInput2 = prompt("Enter second number");
+let ammountPlate = 0;
+const totalAmmountPlate = 5;
 
-const number1 = Number(userInput1);
-const number2 = Number(userInput2);
-
-const operator = prompt(" Enter operator +,  -,  *,  /, %");
-
-switch (operator) {
-  case "+": {
-    const result = number1 + number2;
-    alert(number1 + "+" + number2 + "=" + result);
-    break;
-  }
-  case "-": {
-    const result = number1 - number2;
-    alert(number1 + "-" + number2 + "=" + result);
-    break;
-  }
-  case "*": {
-    const result = number1 * number2;
-    alert(number1 + "*" + number2 + "=" + result);
-    break;
-  }
-  case "/": {
-    const result = number1 / number2;
-    alert(number1 + "/" + number2 + "=" + result);
-    break;
-  }
-  case "%": {
-    const result = number1 % number2;
-    alert(number1 + "%" + number2 + "=" + result);
-    break;
-  }
-  default:
-    break;
+while (ammountPlate < totalAmmountPlate) {
+  ammountPlate++;
+  console.log("washing plate ", ammountPlate);
 }
 
-// const userInputDayOfWeek = `Write day of week:
-// For 1 to 7`;
-
-// const dayOfWeek = prompt(userInputDayOfWeek);
-
-// switch (dayOfWeek) {
-//   case "1":
-//   case "2":
-//   case "3":
-//   case "4":
-//   case "5":
-//     alert("workday");
-//   case "6":
-//   case "7":
-//     alert("Weekend");
-//     break;
-//   default:
-//     alert("There is not day of week");
-//     break;
+// while (ammountPlate>0) {
+//   console.log("Wahsing plate ", ammountPlate);
+//   ammountPlate++;
 // }
