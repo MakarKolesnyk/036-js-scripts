13a114aeda3f16f7d010e608d2969b5dcbc5484e
Makar <kolesnykmakar0@gmail.com>
2025-10-05 18:23:10 +0300

    Add password check with limited attempts

--- index.js
+++ index.js
@@ -15,14 +15,59 @@
 //   }
 // }
 
-const start = 2;
-const end = 7;
+// const start = 1;
+// const end = 5;
 
-let summa = 0;
-for (let i = start; i < end; i++) {
-  // console.log(i);
-  summa += i;
-   console.log(suma);
+// let summa = 0;
+// for (let i = start; i < end; i++) {
+//   // console.log(i);
+//   summa += i;
+//   //  console.log(summa);
+// }
+//   console.log('summa =', summa);
+
+// let mult =1;
+// for (let i =start; i < end; i++) {
+//   mult *=i;
+// }
+//   console.log('mult =', mult);
+
+// const start = 0;
+// const end = 10;
+
+// let mult = 1;
+// let summa = 0;
+
+// for (let i = start; i <= end; i++) {
+//   if (i % 2 === 0) {
+//     summa += i;
+//     console.log(i);
+//   } else {
+//     mult *= i;
+//   }
+// }
+// console.log("summa =", summa);
+// console.log("mult = ", mult);
+
+// const password = "qwe123";
+// const maxTry = 3;
+
+const password = "qwe123";
+const maxTry = 3;
+
+for (let i = 0; i < maxTry; i++) {
+  const userInputPassword = prompt("Введите пароль");
+
+  if (userInputPassword === password) {
+    alert("Добро пожаловать!");
+    break; 
+  } else {
+    const triesLeft = maxTry - i - 1;
+
+    if (triesLeft > 0) {
+      alert("Неверный пароль! Осталось попыток: " + triesLeft);
+    } else {
+      alert("Пароль не работает. Доступ запрещён.");
+    }
+  }
 }
-  console.log('summa' + summa);
-  
