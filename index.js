cc8e8ce532f63cd169a2da59a4096efcd2974445
Makar <kolesnykmakar0@gmail.com>
2025-10-05 17:30:24 +0300

    Add summa in diapozon

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-for (let i = 0; i < 5; i++) {
-  console.log(i);
-}
+// let i = 5;
 
-let i = 0;
-while (i < 5) {
-  console.log(i);
-  i++;
+// while (i > 0) {
+//   console.log(i);
+//   i--;
+// }
+
+// for (let i = 5; i > 0; i--) {
+//   console.log(i);
+// }
+
+// for (let i = 10; i < 50; i++) {
+//   if (i % 7 === 0) {
+//     console.log(i);
+//   }
+// }
+
+const start = 2;
+const end = 7;
+
+let summa = 0;
+for (let i = start; i < end; i++) {
+  // console.log(i);
+  summa += i;
+   console.log(suma);
 }
+  console.log('summa' + summa);
+  
