0596f96024da79d53f765729c51e9196b68aa3bd
Makar <kolesnykmakar0@gmail.com>
2025-10-06 09:11:13 +0300

    Add шаблонні рядки

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-const start = 10;
-const end = 20;
-const secretNumber = 17;
-
-for (let i = start; i < end; i++) {
-  if (i === secretNumber) {
-    continue;
-  }
-  console.log(i);
-}
+const number1 = 1;
+const number2 = 7;
+const result = number1 * number2;
 
 while (true) {
-  const userInput = prompt("Enter number");
-  if (userInput === null) {
+  const userInput = prompt(`how much will it be
+    ${number1} * ${number2} ?`);
+  if (userInput === null || userInput.trim()==='') {
     alert("Dont cancel");
     continue;
     alert("After continue") // this row will not work
   }
-  if (Number(userInput === secretNumber)) {
-    alert("You Win");
+  if (Number(userInput === result)) {
+    alert("Win answer = " + result);
     break;
   }
+  alert("Try more")
 }
